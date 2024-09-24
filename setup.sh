65accf12d63ea42d53b4ca584eb10f33456a3f3c
zarool <karol.zachura01@gmail.com>
2024-09-24 21:35:20 +0200

    Preparing for migration to new structure

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!bin/bash
+##################################
+# Instal necessary dependencies  #
+# for futurelab project          #
+##################################
+
+sudo apt install python3-pil python3-pil.imagetk
+python3 -m pip install customtkinter numpy matplotlib pyserial
