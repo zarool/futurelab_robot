fc125713e0cc47d27683389acecff8c8b8ca054f
zachurak <karol.zachura@stazysta.comarch.pl>
2024-09-25 15:09:38 +0200

    Added basic structure - need to refactor most of components

---
+++ main.py
@@ -0,0 +1,5 @@
+from app.gui import App
+
+app = App()
+
+app.run()
--- robot/kinematics.py
+++ robot/kinematics.py
@@ -1,26 +1,6 @@
 import math
 import numpy as np
-from robot.matrics import Matrix
-
-def compute_end_effector_position(theta1, theta2, theta3, theta4, a3=152.794, a4=157.76, a5=90):
-    T1_matrix = Matrix.T1(theta1)
-    T2_matrix = np.dot(T1_matrix, Matrix.T2(theta2))
-    T3_matrix = np.dot(T2_matrix, Matrix.T3(theta3, a3))
-    T4_matrix = np.dot(T3_matrix, Matrix.T4(theta4, a4))
-    T5_matrix = np.dot(T4_matrix, Matrix.T5(a5))
-    
-    z1 = T1_matrix[2, 3]
-    z2 = T2_matrix[2, 3]
-    z3 = T3_matrix[2, 3]
-    z4 = T4_matrix[2, 3]
-    z5 = T5_matrix[2, 3]
-    
-    if  z3 <= 0 or z4 <= 0 or z5 <= 0:
-        raise ValueError("Jeden z punktów znajduje się poniżej lub na poziomie 0 w osi Z")
-    
-    return T5_matrix[0, 3], T5_matrix[1, 3], z5
-
-
+from robot.matrics import compute_end_pos
 
 
 def inverse_kinematics(x, y, z, a2=152.794, a3=157.76, a4=90):
@@ -99,7 +79,7 @@ def inverse_kinematics(x, y, z, a2=152.794, a3=157.76, a4=90):
     #     pos1 = int(((math.degrees(theta1) / -360) * 2 * steps_per_revolution) + 2048)
     else:
         pos4 = 0
-    compute_end_effector_position(theta1, theta2, theta3, theta4)
+    compute_end_pos(theta1, theta2, theta3, theta4)
 
     if(pos3 < 150):
         valid_position = 0
